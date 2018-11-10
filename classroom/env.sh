7b418353f1425834a872fd41ffedaa021dc38fdb
jrigsbee <jrigsbee@redhat.com>
2018-11-10 10:42:35 -0500

    Updates for OCP 3.11 including new casl-ansible requirement for 3.11

---
+++ classroom/env.sh
@@ -0,0 +1,5 @@
+export RHSM_USER=<subscription username>
+export RHSM_PASSWD=<subscription password>
+export RHSM_POOL=<POOL ID for EMPLOYEE SKU>
+export REG_USERNAME=<registry.redhat.io service account username>
+export REG_TOKEN=<registry.redhat.io service account token>
--- classroom/provision.sh
+++ classroom/provision.sh
@@ -1,15 +1,17 @@
 #!/bin/sh
+source env.sh
+cp -a galaxy/casl-ansible/inventory/sample.aws.example.com.d galaxy/casl-ansible/inventory/do500.aws.d
+cp -a inventory_ocp galaxy/casl-ansible/inventory/do500.aws.d/inventory
 docker run -u `id -u` \
       -v $HOME/.ssh/glsdemo2.pem:/opt/app-root/src/.ssh/id_rsa:Z \
       -v $HOME/Sources/do500/galaxy/:/tmp/src:Z \
+      -v $HOME/aws-credentials.csv:/opt/app-root/src/aws-credentials.csv:Z \
       -e RHSM_USER=$RHSM_USER \
       -e RHSM_PASSWD=$RHSM_PASSWD \
       -e RHSM_POOL=$RHSM_POOL \
       -e REG_USERNAME=$REG_USERNAME \
       -e REG_TOKEN=$REG_TOKEN \
-      -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID \
-      -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY \
-      -e INVENTORY_DIR=/tmp/src/casl-ansible/inventory/sample.aws.example.com.d/inventory \
+      -e INVENTORY_DIR=/tmp/src/casl-ansible/inventory/do500.aws.d/inventory \
       -e PLAYBOOK_FILE=/tmp/src/casl-ansible/playbooks/openshift/end-to-end.yml \
       -e OPTS="-e aws_key_name=glsdemo2" -t \
-      redhatcop/installer-aws
+      redhatcop/casl-ansible 
