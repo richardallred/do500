e8159f67b8e53aaa33e03d4595e2011e3bd2523d
jrigsbee <jrigsbee@redhat.com>
2018-11-16 17:38:17 -0500

    final testing of ocp 3.11

--- classroom/deprovision.sh
+++ classroom/deprovision.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
+cp ansible.cfg galaxy/casl-ansible/
 docker run -u `id -u` \
       -v $HOME/.ssh/glsdemo2.pem:/opt/app-root/src/.ssh/id_rsa:Z \
       -v $HOME/Sources/do500/classroom/galaxy/:/tmp/src:Z \
       -v $HOME/aws-credentials.csv:/opt/app-root/src/aws-credentials.csv:Z \
       -e INVENTORY_DIR=/tmp/src/casl-ansible/inventory/do500.aws.d/inventory/ \
+      -e ANSIBLE_CONFIG=/tmp/src/casl-ansible/ansible.cfg \
       -e OPTS="-e aws_key_name=glsdemo2" \
       -e PLAYBOOK_FILE=/tmp/src/casl-ansible/playbooks/openshift/delete-cluster.yml -it \
       quay.io/redhat-cop/casl-ansible
--- classroom/provision-idm.sh
+++ classroom/provision-idm.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 cp -a inventory_idm galaxy
+cp ansible.cfg galaxy/casl-ansible/
 docker run -u `id -u` \
       -v $HOME/.ssh/glsdemo2.pem:/opt/app-root/src/.ssh/id_rsa:Z \
-      -v $HOME/Sources/do500/galaxy/:/tmp/src:Z \
+      -v $HOME/Sources/do500/classroom/galaxy/:/tmp/src:Z \
       -v $HOME/aws-credentials.csv:/opt/app-root/src/aws-credentials.csv:Z \
-      -e INVENTORY_DIR=/tmp/src/inventory_idm/ \
+      -e ANSIBLE_CONFIG=/tmp/src/casl-ansible/ansible.cfg \
+      -e INVENTORY_DIR=/tmp/src/inventory_idm \
       -e PLAYBOOK_FILE=/tmp/src/casl-ansible/galaxy/infra-ansible/playbooks/provision-idm-server/main.yml \
       -e OPTS="-e aws_key_name=glsdemo2" -it \
       quay.io/redhat-cop/casl-ansible
